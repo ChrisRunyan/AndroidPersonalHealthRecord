13ceba6f01bc52cefb480d951d0977e76812d89c
Jonathan Daniel <jonathans.daniel@yahoo.com>
2018-09-22 17:24:07 -0500

    medicationing

--- app/src/main/java/com/example/jonat/personalhealthrecord/MainActivity.java
+++ app/src/main/java/com/example/jonat/personalhealthrecord/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jonat.personalhealthrecord;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -7,6 +8,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
@@ -42,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.i("CHECK", "Application started");
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -66,6 +70,35 @@ public class MainActivity extends AppCompatActivity {
         context.add(0, view.getId(), 0, R.string.addMenu_procedures);
         context.add(0, view.getId(), 0, R.string.addMenu_allergies);
     }
+
+    public boolean onContextItemSelected(final MenuItem item) {
+        // Handle item selection
+        Log.i("CHECK", "Entered the fab");
+
+        if (getString(R.string.addMenu_symptoms).contentEquals(item.getTitle())) {
+            return true;
+        }
+
+        else if (getString(R.string.addMenu_medications).contentEquals(item.getTitle())) {
+            Log.i("CHECK", "Entered the fab and first case");
+            Intent lstn_intent1 = new Intent(getApplicationContext(), AddMedicationsActivity.class);
+            startActivity(lstn_intent1);
+            return true;
+        }
+
+        else if (getString(R.string.addMenu_procedures).contentEquals(item.getTitle())) {
+            return true;
+        }
+
+        else if (getString(R.string.addMenu_allergies).contentEquals(item.getTitle())) {
+            return true;
+        }
+
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     private void openActivity(Class c) {
         AppCompatActivity me = MainActivity.this;
